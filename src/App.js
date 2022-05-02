b51f952cc41aafbd0887817142c2c0af4c8c61e3
Glen1986 <escobar.glen@gmail.com>
2022-05-02 11:24:07 -0300

    formik-app-custom-component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Formik, Form, Field, ErrorMessage } from 'formik'
+import TextInput from './components/textInput'
 
 const validate = (values) => {
     const errors = {}
@@ -23,6 +24,7 @@ function App() {
     return (
         <Formik
             initialValues={{
+                nombre: '',
                 name: '',
                 lastname: '',
                 email: '',
@@ -36,13 +38,7 @@ function App() {
                     style={{ display: 'block' }}
                     onSubmit={formik.handleSubmit}
                 >
-                    <label>Nombre</label>
-                    <Field name="name" type="text" className="input" />
-                    <Field name="select" as="select" className="input">
-                        <option value="weed">weed</option>
-                        <option value="cafe">café</option>
-                    </Field>
-                    <ErrorMessage name="name" />
+                    <TextInput name="name" label="Nombre" />
                     <br />
                     <label>Apellido</label>
                     <Field name="lastname" type="text" />
